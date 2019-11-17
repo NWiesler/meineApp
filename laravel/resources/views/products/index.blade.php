20dbc55192fc01758cdd3746fe559ed95db74202
NWiesler <48588140+NWiesler@users.noreply.github.com>
2019-11-17 19:59:06 +0100

    Implementierung der Navigation auf Unterseiten
    Die Navigation kann nun auch auf der myfridge Seite verwendet werden.

--- laravel/resources/views/products/index.blade.php
+++ laravel/resources/views/products/index.blade.php
@@ -2,54 +2,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left my-4">
-                <h1>What's in my fridge?</h1>
-            </div>
-            <div class="pull-right mb-4">
-                <a class="btn btn-success" href="{{ route('products.create') }}"> Lebensmittel hinzufügen</a>
+    <div class="container">
+        
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left my-4 text-info">
+                    <h1>{{ Auth::user()->name }}'s fridge</h1>
+                </div>
+                <div class="pull-right mb-4">
+                    <a class="btn btn-success" href="{{ route('products.create') }}"> Lebensmittel hinzufügen</a>
+                </div>
             </div>
         </div>
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+
+        <table class="table table-bordered">
+            <tr>
+                <th>Nr.</th>
+                <th>Produktname</th>
+                <th>Beschreibung</th>
+                <th>Mindestens haltbar bis</th>
+                <th width="300px">Aktion</th>
+            </tr>
+            @foreach ($products as $product)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->detail }}</td>
+                <td>{{ $product->MHD }}</td>
+                <td>
+                    <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+
+                        <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Details</a>
+
+                        <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Bearbeiten</a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger">Löschen</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+
+        {!! $products->links() !!}
+
     </div>
-
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-    <table class="table table-bordered">
-        <tr>
-            <th>Nr.</th>
-            <th>Produktname</th>
-            <th>Beschreibung</th>
-            <th>Mindestens haltbar bis</th>
-            <th width="280px">Aktion</th>
-        </tr>
-        @foreach ($products as $product)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $product->name }}</td>
-            <td>{{ $product->detail }}</td>
-            <td>{{ $product->MHD }}</td>
-            <td>
-                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-
-                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Details</a>
-
-                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Bearbeiten</a>
-
-                    @csrf
-                    @method('DELETE')
-
-                    <button type="submit" class="btn btn-danger">Löschen</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
-    {!! $products->links() !!}
 
 @endsection
--- laravel/resources/views/products/layout.blade.php
+++ laravel/resources/views/products/layout.blade.php
@@ -5,19 +5,15 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<style>
-table{background-color: #fff;color: black;}
-table strong{color: black;}
+<style> 
+table{background-color: #DDD ;color: black;}
+/*table strong{color: black;}
 h2,h1{color: #fff; font-family:'Raleway', sans-serif;}
 strong,p{color: #fff;}
-th,td{font-family:'Raleway', sans-serif;}
-body {background-image: url('https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
-    background-size:cover;}
-</style>
+th,td{font-family:'Raleway', sans-serif;}*/
 
-    <div class="container">
-     @yield('content')
-    </div>
+</style>
+    
     
 </body>
 </html>
