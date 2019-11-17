baaa1d020976200313173a785a8cb7637a09b5f3
NWiesler <48588140+NWiesler@users.noreply.github.com>
2019-11-17 21:47:08 +0100

    Versuch Fehler mit MHD beheben
    Versuch mit anderer Methode den Fehler zu beheben

--- laravel/database/migrations/2019_11_13_064827_create_products_table.php
+++ laravel/database/migrations/2019_11_13_064827_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('detail');
-            $table->date('MHD');
+            $table->text('MHD');
             $table->timestamps();
         });
     }
