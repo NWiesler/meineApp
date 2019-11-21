dd71a30ab88e5443beea0c58e0d510e2c722ce69
NWiesler <48588140+NWiesler@users.noreply.github.com>
2019-11-21 07:42:37 +0100

    "MHD"-DB Fehler beheben
    Der Fehler konnte durch einen anderen Befehl die Migrations zu aktualisieren behoben werden. Dafür wurde "docker-compose exec app php artisan migrate:refresh" verwendet. Außerdem wurde die MHD Methode durch eine passendere ersetzt.

--- laravel/database/migrations/2019_11_13_064827_create_products_table.php
+++ laravel/database/migrations/2019_11_13_064827_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('detail');
-            $table->text('MHD');
+            $table->date('MHD');
             $table->timestamps();
         });
     }
